347ea2f51d099b8c449f97761f29f30ca1f431a0
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-09-02 06:05:40 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,20 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    def __init__(self, name,breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
+
+
+    @classmethod
+    def create_table(cls):
+        sql = '''
+            CREATE TABLE IF DOES NOT EXIST dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+            );
+        '''
+        CURSOR.execute(sql)
+        CONN.commit()
